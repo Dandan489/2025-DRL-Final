7f447720ad727176ec283a3c16779c1fbc283271
unicorn.chk@gmail.com <unicorn.chk@gmail.com>
2025-06-02 10:28:35 +0800

    fix config

--- light_mappo/config.py
+++ light_mappo/config.py
@@ -340,7 +340,7 @@ def get_config():
     parser.add_argument(
         "--entropy_coef",
         type=float,
-        default=0.1,
+        default=0.01,
         help="entropy term coefficient (default: 0.01)",
     )
     parser.add_argument(
